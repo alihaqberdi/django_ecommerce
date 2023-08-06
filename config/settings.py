c261ea1dafe9f33997a29c63b2b275d9bcd3b2ed
alihaqberdi <alhppypro@gmail.com>
2023-08-06 16:40:26 +0500

    database settings

--- config/settings.py
+++ config/settings.py
@@ -88,13 +88,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "shopping",
-        "USER": "postgres",
-        "PASSWORD": "lopsed2211tyg",
-        "HOST": "db",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite7',
     }
 }
 
